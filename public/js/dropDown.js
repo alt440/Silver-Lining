22c9ca4495d4be33c616518ed69e30acefb7deb2
bob <alexandre.therrien3@gmail.com>
2019-01-26 18:02:53 -0500

    Completed dropdown

---
+++ public/js/dropDown.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+
+  $('#firstSelect').click(function(){
+    $('#buttonDropdown').html("Asia/Shanghai");
+  })
+
+  $('#secondSelect').click(function(){
+    $('#buttonDropdown').html("Asia/Kolkata");
+  })
+
+  $('#thirdSelect').click(function(){
+    $('#buttonDropdown').html("America/New_York");
+  })
+
+  $('#buttonChangeTimeZone').click(function(){
+    //set a variable in local storage
+    localStorage.setItem("timezone", $('#buttonDropdown').html());
+    //refresh the page
+    location.reload();
+  })
+
+});
--- public/js/timeOfDayArray.ts
+++ public/js/timeOfDayArray.ts
@@ -1,26 +1,73 @@
 //Activities of the day array
+//must follow the timeArray order!
 var activitiesArray = [
+  "coffee.jpg",
   "breakfast.jpg",
-  "trash.jpg"
+  "trash.jpg",
+  "reusableContainers.jpg",
+  "bicycle.jpg",
+  "carpool.jpg",
+  "food.jpg",
+  "restaurant.png",
+  "packagedFood.jpg",
+  "reusableBags.jpg",
+  "plasticUtensils.jpg",
+  "lightbulb.jpg"
 ];
 
 //Time of day events are happening
+//must be in increasing order!!
 var timeArray = [
-  "16:00",
-  "15:59"
+  "7:00",
+  "7:30",
+  "8:00",
+  "8:15",
+  "8:30",
+  "8:45",
+  "11:30",
+  "12:00",
+  "17:00",
+  "17:30",
+  "18:30",
+  "19:00"
 ];
 
+//must follow the timeArray order!
 var descriptionArray = [
+  "Why use many cups when you can use only one?",
   "Boiled food takes more energy than cereals!",
-  "Do not forget to throw your banana to compost!"
+  "Do not forget to throw your banana to compost!",
+  "Use containers instead of plastic bags",
+  "Scale back car usage",
+  "Do some carpooling if you want to drive to work",
+  "Still food on your plate? Donate it to a food kitchen!",
+  "Ask for bringing back your food leftovers home",
+  "Buy less packaged food",
+  "Bring your reusable bags to supermarkets",
+  "Abandon the plastic utensils for your metal utensils",
+  "Turn off the light when you do not need it"
 ];
 
 //returns the winning index (event that is closest to the current time)
 function getTimeOfDayEvent(){
-  var date = new Date();
+
+  var date;
+
+  if(localStorage.getItem("timezone").localeCompare("Select Time Zone")!=0 &&
+      localStorage.getItem("timezone") != undefined){
+        date = new Date().toLocaleString("en-US", {timeZone: localStorage.getItem("timezone")});
+        date = new Date(date);
+        /*date.toLocaleString('en-US',
+            {timeZone: localStorage.getItem("timezone")});*/
+      }
+  else{
+    date = new Date();
+  }
 
   var hours = date.getHours();
   var minutes = date.getMinutes();
+
+  alert(hours+":"+minutes);
 
   //closest index above the actual time
   var winningIndex = 0;
@@ -39,28 +86,37 @@ function getTimeOfDayEvent(){
       //if the time of day and the event are at the same hour, then minutes need
       //to be greater or equal
       if(hourInArray==hours){
-        if(hourInArray==winningHour && minutesInArray>=minutes
+        //if the current hour is the same as the hour of event, then only need
+        //to check the minutes
+        //first statement because must be at a later time
+        //second statement because it must be closest to the current time
+        if(minutesInArray>=minutes
           && minutesInArray < winningMinutes){
             winningIndex = i;
             winningHour = hourInArray;
             winningMinutes = minutesInArray;
           }
-        else if(winningHour>=hourInArray && minutesInArray>=minutes){
-          winningIndex = i;
-          winningHour = hourInArray;
-          winningMinutes = minutesInArray;
-        }
       }
 
       else{
         //if they do not have the same hour and the current index has a lower
         //hour, then it is closest to the current time
-        winningIndex = i;
-        winningHour = hourInArray;
-        winningMinutes = minutesInArray;
+        if(hourInArray==winningHour){
+          if(minutesInArray<winningMinutes){
+            winningIndex = i;
+            winningHour = hourInArray;
+            winningMinutes = minutesInArray;
+          }
+        }
+        else{
+          winningIndex = i;
+          winningHour = hourInArray;
+          winningMinutes = minutesInArray;
+        }
+
       }
     }
   }
-
+  alert(winningHour+":"+winningMinutes);
   return winningIndex;
 }
