4a3cb12f620376421fb97642a3aee95367d8c42d
Speedst1998 <mtahar1998@gmail.com>
2019-01-26 23:10:31 -0500

    working on cars emissions

--- server.js
+++ server.js
@@ -12,32 +12,92 @@ app.set('view engine', 'ejs');
 
 router.get('/', function(req, res){
 
-  var response = Request.get("https://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year=2012",
+
+    //var menuItems = result.menuItems;
+     res.render('pages/homePage.ejs');
+   });
+   //console.log("response" + res);
+// //      var testdata = {
+// //       tatas: [
+// //        {tata:'tata', value:'nuto'},
+// //        {tata:'tata', value:'nuta'},
+// //        {tata:'samer'}
+// //      ]
+// //    }
+// //      var query = jsonQuery('*tatas[tata=tata].text', {
+// //   testdata: testdata
+ });
+// // var data = {
+// //   people: [
+// //     {name: 'Matt', country: 'NZ'},
+// //     {name: 'Pete', country: 'AU'},
+// //     {name: 'Mikey', country: 'NZ'}
+// //   ]
+// // }
+//
+// // var query = jsonQuery('menuItem[0].text', {
+// //   data: menuItems
+// // }).value
+// //     console.log("query" + query);
+// //     console.log("items" + JSON.stringify(menuItems));
+// // });
+//
+//   //console.log(res);//, {
+//   //   response: response
+//   // });
+//
+
+})
+
+router.post('/',async(req,res)=>{
+  // var vehicles = Request.get("https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year="
+  // +req.body.year+"&make="+req.body.make+"&model="+req.body.model,
+  var vehicles = Request.get("https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2012&make=Honda&model=Fit",
    function (error, response, body) {
-              if (error) {
-                  throw error;
-              }
-              parseString(body, function (err, result) {
-    console.dir(result.menuItems.menuItem[0].text);
-     res.render('pages/homePage.ejs',{makes:result.menuItems.menuItem});
-     var testdata = {
-      tatas: [
-       {tata:'tata', value:'nuto'},
-       {tata:'tata', value:'nuta'},
-       {tata:'samer'}
-     ]
-   }
-     var query = jsonQuery('tatas[tata=tata].text', {
-  testdata: testdata
-});
-    console.log("query" + JSON.stringify(query));
-});
+             if (error) {
+                 throw error;
+             }
+             console.log("response" + response);
+              response = parseString(body, function (err, result) {
+   console.dir(JSON.stringify(result));
+   var emissions = []
+   // for(i = 0; i < result.menuItems.menuItem.length; i++){
+   //   var emission = Request.get("https://www.fueleconomy.gov/ws/rest/vehicle/emissions/"
+   //   +result.menuItems.menuItem[i],
+   //   function (error, response, body) {
+   //             if (error) {
+   //                 throw error;
+   //             }
+   //
+   //             var emission = parseString(body, function (err, result) {});
+   //             emissions.push(emission);
+   //             console.log("emission" + emission);
+   //             if(i == result.menuItems.menuItem.length){
+   //
+   //             }
+   // });
+   //console.log("emissions" + emissions);
+   res.redirect('/',{items:result.menuItems});
+ });
+ console.log("Parsed resonse"+ response );
 
-  //console.log(res);//, {
-  //   response: response
-  // });
+ //end parseString
+ // for(i = 0; i < result.menuItems.menuItem.length; i++){
+ //   var emission = Request.get("https://www.fueleconomy.gov/ws/rest/vehicle/emissions/"
+ //   +result.menuItems.menuItem[i],
+ //   function (error, response, body) {
+ //             if (error) {
+ //                 throw error;
+ //             }
+ //
+ //             var emission = parseString(body, function (err, result) {});
+ //             emissions.push(emission);
+ //             console.log("emission" + emission);
+ //             if(i == result.menuItems.menuItem.length){
+ //
+ //             }
+ // });
 });
-
 })
 
 router.get('/otherPage.ejs', function(req, res){
